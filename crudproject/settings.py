faf9db7ea80389efef492986d9f90256414d830f
Roshni Yadav <144584696+roshniyadav00001@users.noreply.github.com>
2025-05-27 15:54:07 +0530

    Update settings.py

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -68,7 +68,11 @@ WSGI_APPLICATION = 'crudproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ' ',
+        'USER': ' ',
+        'PASSWORD':' ',
+        'HOST': ' ',
+        'PORT': ' ',
     }
 }
 
